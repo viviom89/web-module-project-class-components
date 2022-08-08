3898231a5d08a5e84afe49a15f420f48d8d4afe3
Chhorvy Om <c_om89@live.com>
2022-08-08 13:18:29 -0700

    Added a list element to App js on line 9

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,13 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+      <h1>Todo App</h1>
+
+      <ul>
+        <li>Walk the dog</li>
+        <li>Learn React</li>
+        <li>Have fun</li>
+      </ul>
       </div>
     )
   }
